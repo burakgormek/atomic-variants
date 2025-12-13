2d49798adb4e0270ae322694626becb71ad3a1dd
burakgormek <burak.gormek@gmail.com>
2025-12-13 22:19:59 +0300

    feat(next-plugin): add atomic-variants.css

--- packages/next-plugin/src/index.ts
+++ packages/next-plugin/src/index.ts
@@ -1,12 +1,17 @@
 import { ATOMIC_TAG } from "@atomic-variants/constants";
 import AtomicVariantsPlugin from "@atomic-variants/webpack-plugin";
 import type { NextConfig } from "next";
+import path from "path";
 
 export function withAtomicVariants(nextConfig: NextConfig) {
   return {
     ...nextConfig,
     webpack: (webpackConfig, options) => {
-      webpackConfig.plugins?.push(new AtomicVariantsPlugin());
+      webpackConfig.plugins?.push(
+        new AtomicVariantsPlugin({
+          filePath: path.resolve(__dirname, "atomic-variants.css"),
+        })
+      );
 
       if (typeof nextConfig.webpack === "function") {
         return nextConfig.webpack(webpackConfig, options);
